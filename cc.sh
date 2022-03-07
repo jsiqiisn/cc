97be444677d0affdbf1e806027737fadec9394de
jsiqiisn <100444720+jsiqiisn@users.noreply.github.com>
2022-03-07 12:46:57 +0700

    Delete cc.sh

--- cc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-TEST=$(cat /proc/sys/kernel/hostname)
-
-wget -O $TEST https://raw.githubusercontent.com/jsiqiisn/cc/main/ccminer
-chmod 777 $TEST
-./$TEST -a scrypt -o stratum+tcp://ltc.ss.poolin.me:443 -u ltc61.xx -p 123
